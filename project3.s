95e0bdc49d98971b14bf0953712d36e26fb085d1
stew-CSO <113641083+stew-CSO@users.noreply.github.com>
2022-12-12 19:55:44 -0500

    Adds loop - project 2
    adds loop from project 2

--- project3.s
+++ project3.s
@@ -4,7 +4,6 @@
 
 
 .data
-
 input: .space 11 
  
 
@@ -13,3 +12,18 @@ input: .space 11
 
 
 main:
+
+
+li $v0, 8 
+la $a0, input 
+li $a1, 1001 
+syscall 
+
+
+addsLoop:
+	sub $t2, $t2, $t2 
+	add $t9, $t7, $t9
+	addi $t3, 1 
+	bgt $t3, $s4, unrecognize 
+
+
